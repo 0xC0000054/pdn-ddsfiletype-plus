1e71912951743e303ffc1597004182955d4644c1
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2025-04-12 23:25:47 -0600

    Update copyright year

--- src/Localization/BuiltinStringResourceManager.cs
+++ src/Localization/BuiltinStringResourceManager.cs
@@ -3,7 +3,7 @@
 // This file is part of pdn-ddsfiletype-plus, a DDS FileType plugin
 // for Paint.NET that adds support for the DX10 and later formats.
 //
-// Copyright (c) 2017-2024 Nicholas Hayes
+// Copyright (c) 2017-2025 Nicholas Hayes
 //
 // This file is licensed under the MIT License.
 // See LICENSE.txt for complete licensing and attribution information.
--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@
 // This file is part of pdn-ddsfiletype-plus, a DDS FileType plugin
 // for Paint.NET that adds support for the DX10 and later formats.
 //
-// Copyright (c) 2017-2024 Nicholas Hayes
+// Copyright (c) 2017-2025 Nicholas Hayes
 //
 // This file is licensed under the MIT License.
 // See LICENSE.txt for complete licensing and attribution information.
@@ -22,7 +22,7 @@ using System.Runtime.Versioning;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("null54")]
 [assembly: AssemblyProduct("DdsFileTypePlus")]
-[assembly: AssemblyCopyright("Copyright © 2017-2024 Nicholas Hayes (aka null54)")]
+[assembly: AssemblyCopyright("Copyright © 2017-2025 Nicholas Hayes (aka null54)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
